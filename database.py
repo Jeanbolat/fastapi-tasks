655de3e8d8be1e8d3449671e76f2d3364f7430f5
Jeanbolat <kagantegin@gmail.com>
2026-08-11 18:14:43 +0500

    Prepare app for Render deployment

--- database.py
+++ database.py
@@ -1,19 +1,37 @@
 import os
 
 from dotenv import load_dotenv
-
-load_dotenv()
 from sqlalchemy import URL, create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DATABASE_URL = URL.create(
-    drivername="postgresql+psycopg",
-    username="postgres",
-    password=os.environ["DATABASE_PASSWORD"],
-    host=os.getenv("DATABASE_HOST", "localhost"),
-    port=5432,
-    database="tasks_db",
-)
+load_dotenv()
+
+database_url = os.getenv("DATABASE_URL")
+
+if database_url:
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace(
+            "postgres://",
+            "postgresql+psycopg://",
+            1,
+        )
+    elif database_url.startswith("postgresql://"):
+        database_url = database_url.replace(
+            "postgresql://",
+            "postgresql+psycopg://",
+            1,
+        )
+
+    DATABASE_URL = database_url
+else:
+    DATABASE_URL = URL.create(
+        drivername="postgresql+psycopg",
+        username="postgres",
+        password=os.environ["DATABASE_PASSWORD"],
+        host=os.getenv("DATABASE_HOST", "localhost"),
+        port=5432,
+        database="tasks_db",
+    )
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
